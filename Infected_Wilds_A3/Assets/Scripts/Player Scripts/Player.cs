b76e727d740781ddaa6ff2b2e20300104bf65d06
Kayy <157222013+Karabs2006@users.noreply.github.com>
2025-06-12 12:53:57 +0200

    Tower Sizing

--- Infected_Wilds_A3/Assets/Scripts/Player Scripts/Player.cs
+++ Infected_Wilds_A3/Assets/Scripts/Player Scripts/Player.cs
@@ -172,7 +172,7 @@ public class Player : MonoBehaviour
 
         player.color = Color.red;
 
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.2f);
 
         player.color = originalColor;
     }
