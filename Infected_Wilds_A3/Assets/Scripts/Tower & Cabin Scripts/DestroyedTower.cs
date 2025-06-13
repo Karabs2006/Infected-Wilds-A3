72586d25b91e327721d22b4352087654784544f9
Kayy <157222013+Karabs2006@users.noreply.github.com>
2025-06-13 13:31:21 +0200

    Added Remaining Dialogues + Wolf Spawn

---
+++ Infected_Wilds_A3/Assets/Scripts/Tower & Cabin Scripts/DestroyedTower.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class DestroyedTower : MonoBehaviour
+{
+
+    public GameObject dialogue;
+
+    public Collider2D towerCollider;
+
+    public bool isDestroyed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        dialogue.SetActive(false);
+        isDestroyed = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        if (isDestroyed)
+        {
+            towerCollider.enabled = true;
+            print("Tower is destroyed");
+
+        }
+
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D trigger)
+    {
+        if (trigger.CompareTag("Player"))
+        {   
+            Time.timeScale = 0f;
+            dialogue.SetActive(true);
+
+        }
+    }
+
+
+    public void Next()
+    {
+
+        Time.timeScale = 1f;
+        dialogue.SetActive(false);
+        Destroy(towerCollider);
+    }
+}
