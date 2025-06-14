3f5625cd8083203c065e79b507b13a3db162668a
Kayy <157222013+Karabs2006@users.noreply.github.com>
2025-06-14 14:32:46 +0200

    Pause Menu Fix + Flashing Effect

--- Infected_Wilds_A3/Assets/Scripts/UI Scripts/PauseGame.cs
+++ Infected_Wilds_A3/Assets/Scripts/UI Scripts/PauseGame.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class PauseGame : MonoBehaviour
 {
     public GameObject pauseMenu;
+
+    public GameObject text;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,15 +21,28 @@ public class PauseGame : MonoBehaviour
         {
             Time.timeScale = 0f;
             pauseMenu.SetActive(true);
+            StartCoroutine(FlashRoutine());
         }
 
+    }
+    
+    private IEnumerator FlashRoutine()
+    {
+
+        while (pauseMenu.activeSelf) // Keep flashing while paused
+        {
+            text.SetActive(true);
+            yield return new WaitForSecondsRealtime(0.4f);
+            text.SetActive(false);
+            yield return new WaitForSecondsRealtime(0.4f);
+        }
     }
 
     public void Resume()
     {
         Time.timeScale = 1f;
         pauseMenu.SetActive(false);
-        
+
 
     }
 
