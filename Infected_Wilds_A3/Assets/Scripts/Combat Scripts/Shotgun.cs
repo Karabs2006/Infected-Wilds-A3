1c6e489b4c6c278df016dd43c49fd5fbbcca72a2
Kayy <157222013+Karabs2006@users.noreply.github.com>
2025-06-15 11:50:26 +0200

    Dialogue cabin fix

--- Infected_Wilds_A3/Assets/Scripts/Combat Scripts/Shotgun.cs
+++ Infected_Wilds_A3/Assets/Scripts/Combat Scripts/Shotgun.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
 
-public class Dialogue4 : MonoBehaviour
+public class Shotgun : MonoBehaviour
 {
-
-    public Collider2D dialogueCollider;
+    public PlayerShooting playerShooting;
 
     public GameObject dialogue;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         dialogue.SetActive(false);
+
     }
 
     // Update is called once per frame
@@ -18,26 +17,22 @@ public class Dialogue4 : MonoBehaviour
     {
 
     }
-
- void OnTriggerEnter2D(Collider2D trigger)
+    void OnTriggerEnter2D(Collider2D trigger)
     {
         if (trigger.CompareTag("Player"))
         {
             Time.timeScale = 0f;
             dialogue.SetActive(true);
+            Destroy(gameObject);
         }
 
     }
-
 
     public void Next()
     {
         Time.timeScale = 1f;
         dialogue.SetActive(false);
-        Destroy(dialogueCollider);
-
+       
+       
     }
-    
-   
-
 }
